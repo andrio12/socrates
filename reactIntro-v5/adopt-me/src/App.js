71be7d33efec571a47bbb63e380992e3391a988b
Andri Olafsson <andriolafsson@gmail.com>
2020-07-03 14:29:39 +0000

    eslint config

--- reactIntro-v5/adopt-me/src/App.js
+++ reactIntro-v5/adopt-me/src/App.js
@@ -1,22 +1,16 @@
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed),
-  ]);
-};
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "what-a-div" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Fish",
-      breed: "Shark",
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" }),
-    React.createElement(Pet, { name: "Herkules", animal: "Dog", breed: "Lab" }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Herkúles" animal="Dog" breed="Labrador" />
+      <Pet name="Doink" animal="fish" breed="Shark" />
+      <Pet name="Pepper" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
