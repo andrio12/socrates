37c549591ae8e441a0837137813e914ccccf643a
Andri Olafsson <andriolafsson@gmail.com>
2021-02-12 00:33:29 +0000

    redux and clean code tuts

---
+++ redux-levelup/src/movies/MoviesList.js
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+/* eslint react/no-did-mount-set-state: 0 */
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import styled from 'styled-components';
+import Movie from './Movie';
+import { getMovies } from './actions'
+
+class MoviesList extends PureComponent {
+  componentDidMount() {
+    const { getMovies, isLoaded } = this.props
+    if (!isLoaded) {
+      getMovies()
+    }
+  }
+
+  render() {
+    const { movies, isLoaded, moviesLoadedAt } = this.props
+    const oneHour = 60 * 60 * 1000
+    if (!isLoaded || ((new Date()) - new Date(moviesLoadedAt)) > oneHour) return <h1>Loading movies...</h1>
+    return (
+      <MovieGrid>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </MovieGrid>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  movies: state.movies.movies,
+  isLoaded: state.movies.moviesLoaded,
+  moviesLoadedAt: state.movies.moviesLoadedAt
+})
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({
+  getMovies
+}, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(MoviesList);
+
+const MovieGrid = styled.div`
+  display: grid;
+  padding: 1rem;
+  grid-template-columns: repeat(6, 1fr);
+  grid-row-gap: 1rem;
+`;
---
+++ redux-levelup/src/movies/reducer.js
@@ -0,0 +1,38 @@
+import { GET_MOVIES } from './actions'
+import { GET_MOVIE } from './actions'
+import { RESET_MOVIE } from './actions'
+
+const initialState = {
+    movies: [],
+    moviesLoaded: false,
+    moviesLoadedAt: null,
+    movie: {},
+    movieLoaded: false
+};
+
+export default function (state = initialState, action) {
+    const { type, data } = action;
+    switch (type) {
+        case GET_MOVIES:
+            return {
+                ...state,
+                movies: data,
+                moviesLoaded: true,
+                moviesLoadedAt: new Date()
+            };
+        case GET_MOVIE:
+            return {
+                ...state,
+                movie: data,
+                movieLoaded: true
+            };
+        case RESET_MOVIE:
+            return {
+                ...state,
+                movie: {},
+                movieLoaded: false
+            };
+        default:
+            return state;
+    }
+}
